590a8ece9e25c974fc2770032f6418ffac23b57d
othmane <othman-19@hotmail.fr>
2019-06-10 16:20:58 +0100

    my_each_with_index method added

--- enumerable.rb
+++ enumerable.rb
@@ -7,7 +7,22 @@ module Enumerable
         end
         self
     end
+    
+    #arr=[1,2,3,4,5,6]
+    #arr.my_each{|x| puts "#{x}!" }
+
+    def my_each_with_index
+        i = 0
+        while i < self.size
+            yield(self[i],i)  
+            i+=1      
+        end
+        self
+    end
+
+    #arr=[2,4,6,8,10,12]
+    #arr.my_each_with_index{|x,i| puts "#{x},#{i}!"}
+
 end
 
-arr=[1,2,3,4,5,6];
-arr.my_each{|x| puts "#{x}!" }
+
