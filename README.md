669fcd184ef54a07bd9cfdcdf4076f8583859821
othmane <othman-19@hotmail.fr>
2019-06-11 10:40:11 +0100

    add my-all?, my_any? my_none?, my_count, my_map, my_inject, multiply_els methods and update the readme file

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# enumerable_methods
+# Enumerable_methods
+# Project: enumerable_methods.
+Creating a ruby module that contain the my_enumerable methods using ruby language.
+
+## Prerequisite
+1. Text editor.
+2. Ruby.
+3. Code linter.
+
+##Instruction: how to test the program
+1 - Git clone  https://github.com/othman-19/enumerable_methods.git 
+2 - Navigate to the clone repository.
+3 - Open the file `enumerable.rb` to edit the program.
+4 - After every method you will find a commented lines for tests, just remove the '# 'sign to test the methods.
+4 - Enter `ruby main.rb` in terminal to run the program.
+
+## Contributors
+1.Othmane Naamani[https://github.com/othman-19].
--- enumerable.rb
+++ enumerable.rb
@@ -7,7 +7,6 @@ module Enumerable
         end
         self
     end
-    
     #arr=[1,2,3,4,5,6]
     #arr.my_each{|x| puts "#{x}!" }
 
@@ -19,21 +18,99 @@ module Enumerable
         end
         self
     end
-
     #arr=[2,4,6,8,10,12]
     #arr.my_each_with_index{|x,i| puts "#{x},#{i}!"}
 
     def my_select
         selected=[]
         self.my_each do |item|
-            yield(item) 
             selected.push(item) if yield(item)
         end  
         selected
     end
-end
+    #arr=[2,4,6,8,10,12]
+    #puts arr.my_select{|x| x > 6}
 
-arr=[2,4,6,8,10,12]
-puts arr.my_select{|x| x > 6}
+    def my_all?
+        result=true
+        self.my_each do |item|
+            result = false unless yield(item)
+        end  
+        result
+    end
+    #arr=[2,4,6,8,10,12]
+    #puts arr.my_all?{|x| x > 6}
+    #puts arr.my_all?{|x| x > 1}
+
+    def my_any?
+        result=false
+        self.my_each do |item|
+            result = true if yield(item)
+        end  
+        result
+    end
+    #arr=[2,4,6,8,10,12]
+    #puts arr.my_any?{|x| x > 6}
+    #puts arr.my_any?{|x| x > 15}
+
+    def my_none?
+        result=true
+        self.my_each do |item|
+            result = false if yield(item)
+        end  
+        result
+    end
+    #arr=[2,4,6,8,10,12]
+    #puts arr.my_none?{|x| x > 6}
+    #puts arr.my_none?{|x| x > 12}
+
+    def my_count(item=nil)
+        counter=0
+        if item!=nil
+            self.my_each{ |current| counter += 1 if current == item }
+        elsif block_given?
+            self.my_each{ |current| counter += 1 if yield(current)}
+        else
+            counter = self.length 
+        end
+        counter
+    end
+    #arr=[2,4,2,8,10,12]
+    #puts arr.my_count{|x| x == 2}
+    #puts arr.my_count{|x| x >= 12}
+
+    def my_map(proc=nil)
+        result = []
+        if proc
+            self.my_each do |current|
+                result << proc.call(current)
+            end
+        elsif proc.nil? && block_given?
+            self.my_each do |current|
+                result << yield(current)
+            end
+        end
+        result
+    end
+
+    def my_inject (initial = 0)
+        i = 0
+        accumulator = initial
+        self.my_each do |current|
+            accumulator = yield(accumulator, current)
+        end
+        accumulator
+    end
+
+    def multiply_els
+        self.my_inject(1) {|total, n| total * n}
+    end
+end
+#arr=[2,4,2,8,10,12]
+#puts arr.multiply_els
+
+
+
+
 
 
