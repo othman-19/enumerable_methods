6b7aea1a49503fd71c286f5062d482a3f7ef35bd
othmane <othman-19@hotmail.fr>
2019-06-13 17:34:40 +0100

    add my_map an my_inject methods

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path("../enumerable.rb")
 
 RSpec.describe Enumerable do
   
-=begin 
+#=begin 
   describe "#my_each_with_index" do
       it "find each item with his index" do
         arr=[2,4,6]
@@ -17,9 +17,9 @@ RSpec.describe Enumerable do
         expect(arr.my_each_with_index{|x,i| result.push("#{i} index of #{x}")}).to eql(arr)
       end
   end 
-=end
+#=end
 
-=begin 
+#=begin 
   describe "#my_select" do
       it "return a new array with asked condition in given block" do
         arr=[2,4,6,8,10,12]
@@ -31,9 +31,9 @@ RSpec.describe Enumerable do
         expect(arr).to eql([2,4,6,8,10,12])
       end
   end
-=end
+#=end
 
-=begin 
+#=begin 
   describe "#my_all?" do
       it "return true only if all of the array items respond to the condition in given block" do
         arr=[2,4,6,8,10,12]
@@ -44,9 +44,9 @@ RSpec.describe Enumerable do
         expect(arr.all?{|x| x > 1}).to eql(true)
       end
   end
-=end
+#=end
 
-=begin
+#=begin
   describe "#my_count" do
       it "if no parameter given, it return the number of the array items" do
           arr=[2,4,6,8,10,12]
@@ -65,7 +65,7 @@ RSpec.describe Enumerable do
           expect(arr.my_count{|x| x % 2 == 0}).to eql(4)
       end
   end
-=end
+#=end
 
 #=begin
   describe "#my_map" do
@@ -89,24 +89,16 @@ RSpec.describe Enumerable do
 #=end
 
 #=begin
-describe "#my_inject" do
-it "if no parameter given, it return the number of the array items" do
-    arr=[2,4,6,8,10,12]
-    expect(arr.my_count).to eql(6)
-end
-it "if a parameter given, it return how many times that parameter exist in the array" do
-    arr=[2,4,2,8,10,12]
-    expect(arr.my_count(2)).to eql(2)
-end
-it "if a parameter given, it return how many times that parameter exist in the array" do
-    arr=[2,4,2,8,10,12]
-    expect(arr.my_count(20)).to eql(0)
-end
-it "if a block given, it return how many items that realize the block condition" do
-    arr=[2,4,3,8,10,13]
-    expect(arr.my_count{|x| x % 2 == 0}).to eql(4)
-end
-end
+  describe "#my_inject" do
+    it "it return accumulated result after iterating the array items using a block and an initial value" do
+        arr=[2,4,6,8,10]
+        expect(arr.my_inject(1){ |total, n| total * n }).to eql(3840)
+    end
+    it "it return accumulated result after iterating the array items using a block and an initial value" do
+        arr=[2,4,6,8,10]
+        expect(arr.my_inject(0){ |total, n| total + n }).to eql(30)
+    end
+  end
 #=end
   
 
