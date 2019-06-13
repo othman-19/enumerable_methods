c1677a85b53be4095940476fb556711d8f4fda93
othmane <othman-19@hotmail.fr>
2019-06-13 16:11:05 +0100

    add the my_each_with_index method test

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -2,9 +2,29 @@
 require File.expand_path("../enumerable.rb")
 
 RSpec.describe Enumerable do
-  describe "#add" do
-      it "returns the sum of two numbers" do
-        expect(calculator.add(5, 2)).to eql(7)
+  describe "#my_each_with_index" do
+      it "find each item with his index" do
+        arr=[2,4,6]
+        result=[]
+        arr.my_each_with_index{|x,i| result.push("#{i} index of #{x}")}
+        expect(result).to eql(['0 index of 2','1 index of 4','2 index of 6'])
+      end
+      it "return the original array" do
+        arr=[2,4,6]
+        expect(arr.my_each_with_index{|x,i| result.push("#{i} index of #{x}")}).to eql(arr)
+      end
+  end
+
+  describe "#my_each_with_index" do
+      it "find each item with his index" do
+        arr=[2,4,6]
+        result=[]
+        arr.my_each_with_index{|x,i| result.push("#{i} index of #{x}")}
+        expect(result).to eql(['0 index of 2','1 index of 4','2 index of 6'])
+      end
+      it "return the original array" do
+        arr=[2,4,6]
+        expect(arr.my_each_with_index{|x,i| result.push("#{i} index of #{x}")}).to eql(arr)
       end
   end
 end
