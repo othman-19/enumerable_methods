4f5d3be327a1cf8fcea25d4ed3f073e5e0e0961c
othmane <othman-19@hotmail.fr>
2019-06-14 21:47:10 +0100

    Use blocks as a #let method variables

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -26,9 +26,9 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_select' do
-    let(:selection_block) { arr1.my_select { |x| x > 6 } }
+    let(:select_block) { proc { |x| x > 6 } }
     it 'return a new array with asked condition in given block' do
-      expect(selection_block).to eql([8, 10, 12])
+      expect(arr1.my_select(&select_block)).to eql([8, 10, 12])
     end
     it 'Do not chenge the original array' do
       arr1.my_select { |x| x > 6 }
@@ -39,15 +39,15 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_all?' do
-    let(:only_block1) { arr1.all? { |x| x > 6 } }
-    let(:only_block2) { arr1.all? { |x| x > 1 } }
+    let(:only_block1) { proc{ |x| x > 6 } }
+    let(:only_block2) { proc{ |x| x > 1 } }
     it 'return true only if all of the array items respond to the condition \
       in given block' do
-      expect(only_block1).to eql(false)
+      expect(arr1.all?(&only_block1)).to eql(false)
     end
     it 'return true only if all of the array items respond to the condition \
       in given block' do
-      expect(only_block2).to eql(true)
+      expect(arr1.all?(&only_block2)).to eql(true)
     end
   end
   #=end
@@ -56,7 +56,7 @@ RSpec.describe Enumerable do
   describe '#my_count' do
     let(:arr2) { [2, 4, 2, 8, 10, 12] }
     let(:arr3) { [2, 4, 3, 8, 10, 13] }
-    let(:count_block) { arr3.my_count(&:even?) }
+    let(:count_block) { proc{ |x| x.even? } }
     it 'if no parameter given, it return the number of the array items' do
       expect(arr1.my_count).to eql(6)
     end
@@ -72,7 +72,7 @@ RSpec.describe Enumerable do
     end
     it 'if a block given, it return how many items that realize the \
       block condition' do
-      expect(count_block).to eql(4)
+      expect(arr3.my_count(&count_block)).to eql(4)
     end
   end
   #=end
@@ -80,10 +80,10 @@ RSpec.describe Enumerable do
   #=begin
   describe '#my_map' do
     let(:arr) { %w[a r r a y] }
-    let(:map_block) { arr.my_map { |x| x + x } }
+    let(:map_block) { proc { |x| x + x } }
     it 'it return a new array ,each item modified by the block from \
     the origin method' do
-      expect(map_block).to eql(%w[aa rr rr aa yy])
+      expect(arr.my_map(&map_block)).to eql(%w[aa rr rr aa yy])
     end
   end
   #=end
@@ -91,16 +91,16 @@ RSpec.describe Enumerable do
   #=begin
   describe '#my_inject' do
     let(:arr) { [2, 4, 6, 8, 10] }
-    let(:inject_block1) { arr.my_inject(1) { |total, n| total * n } }
-    let(:inject_block2) { arr.my_inject(0) { |total, n| total + n } }
+    let(:inject_block1) { proc { |total, n| total * n } }
+    let(:inject_block2) { proc { |total, n| total + n } }
 
     it 'it return accumulated result after iterating the array items using \
       a block and an initial value' do
-      expect(inject_block1).to eql(3840)
+      expect(arr.my_inject(1, &inject_block1)).to eql(3840)
     end
     it 'it return accumulated result after iterating the array items using \
       a block and an initial value' do
-      expect(inject_block2).to eql(30)
+      expect(arr.my_inject(0, &inject_block2)).to eql(30)
     end
   end
   #=end
