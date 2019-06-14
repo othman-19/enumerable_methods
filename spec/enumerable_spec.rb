8390a5c3c9ab38d074e0703f7d8af2d2624175b6
stickler-ci <support@stickler-ci.com>
2019-06-14 11:49:09 +0000

    Fixing style errors.

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 # spec/calculator_spec.rb
-#require File.expand_path('./enumerable.rb')
+# require File.expand_path('./enumerable.rb')
 require File.expand_path('../enumerable.rb')
 
 RSpec.describe Enumerable do
   #=begin
   describe '#my_each_with_index' do
-    let(:arr){[2, 4, 6]} 
-    let (:result){[]}
+    let(:arr) { [2, 4, 6] }
+    let (:result) { [] }
     it 'find each item with his index' do
       arr.my_each_with_index { |x, i| result.push("#{i} index of #{x}") }
       expect(result).to eql(['0 index of 2', '1 index of 4', '2 index of 6'])
@@ -24,7 +24,7 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_select' do
-    let(:arr) {[2, 4, 6, 8, 10, 12]}
+    let(:arr) { [2, 4, 6, 8, 10, 12] }
     it 'return a new array with asked condition in given block' do
       expect(arr.my_select { |x| x > 6 }).to eql([8, 10, 12])
     end
@@ -37,7 +37,7 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_all?' do
-    let(:arr) {[2, 4, 6, 8, 10, 12]}
+    let(:arr) { [2, 4, 6, 8, 10, 12] }
     it 'return true only if all of the array items respond to the condition \
       in given block' do
       expect(arr.all? { |x| x > 6 }).to eql(false)
@@ -51,21 +51,21 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_count' do
-    let(:arr1) {[2, 4, 6, 8, 10, 12]}
-    let(:arr2) {[2, 4, 2, 8, 10, 12]}
-    let(:arr3) {[2, 4, 3, 8, 10, 13]}
+    let(:arr1) { [2, 4, 6, 8, 10, 12] }
+    let(:arr2) { [2, 4, 2, 8, 10, 12] }
+    let(:arr3) { [2, 4, 3, 8, 10, 13] }
 
     it 'if no parameter given, it return the number of the array items' do
       expect(arr1.my_count).to eql(6)
     end
     it 'if a parameter given, it return how many times that parameter exist \
       in the array' do
-      
+
       expect(arr2.my_count(2)).to eql(2)
     end
     it 'if a parameter given, it return how many times that parameter exist \
       in the array' do
-      
+
       expect(arr2.my_count(20)).to eql(0)
     end
     it 'if a block given, it return how many items that realize the \
@@ -77,7 +77,7 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_map' do
-  let(:arr)  {%w[a r r a y]}
+    let(:arr) { %w[a r r a y] }
     it 'it return a new array ,each item modified by the block from \
     the origin method' do
       expect(arr.my_map { |x| x + x }).to eql(%w[aa rr rr aa yy])
@@ -87,7 +87,7 @@ RSpec.describe Enumerable do
 
   #=begin
   describe '#my_inject' do
-    let(:arr){[2, 4, 6, 8, 10]}
+    let(:arr) { [2, 4, 6, 8, 10] }
     it 'it return accumulated result after iterating the array items using \
       a block and an initial value' do
       expect(arr.my_inject(1) { |total, n| total * n }).to eql(3840)
